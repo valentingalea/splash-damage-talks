fc1af855a82b287fdf0c67666a21b134745161ba
Valentin Galea <valentingalea@gmail.com>
2019-03-17 10:42:35 +0000

    start experiment with reveal.js

---
+++ reveal/build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+node convert.js
+
+# default way to enable plugins in asciidoctor doesn't seem to work
+# so I cheated and created my own mini mechanism
+sed -e '/dependencies:/ {' -e 'r plugins.js' -e 'd }' -i main.html
+
+#cp main.html ../docs/shaderbox/index.html
---
+++ reveal/plugins.js
@@ -0,0 +1,11 @@
+//
+// plugins
+//
+dependencies: [
+    //,{ src: 'node_modules/reveal.js/plugin/title-footer/title-footer.js'
+    //    ,callback: function() {
+    //        title_footer.initialize("@valentin_galea | Splash Damage");
+    //    }
+    //}
+
+    //, // default plugins past this point; also add new-line
