1d5d8d893a84258a5f0e51e7aaa85bfd3711ad87
valentingalea <valentingalea@gmail.com>
2019-03-19 00:05:24 +0000

    comment for future clarity

--- reveal/custom-fragments.js
+++ reveal/custom-fragments.js
@@ -12,19 +12,17 @@ function dealWithFragments (fragment) {
     var iframe = document.querySelector("div.slide-background.present > iframe:nth-child(1)");
     if (!iframe) return;
 
-    //var evt = new Event('keypress');
-    //evt.keyCode = '39'; // right arrow
-    //iframe.contentWindow.document.body.dispatchEvent(evt);
-
+    // add 1 because we skip the start aka overview
     const id = Number(fragment.getAttribute ('data-fragment-index')) + 1;
+
+    // format of iframe url is: index.html#X.Y
     const href = iframe.contentWindow.location.href;
     const h = href.indexOf('#')
-    const new_href = href.substr(0, h) + '#0.' + id;
-    iframe.contentWindow.location.href = new_href;
-    console.log(new_href);
+    const new_href = href.substr(0, h + 2) + '.' + id;
     
+    iframe.contentWindow.location.href = new_href;
+    //console.log(new_href);
+    
+    //TODO: why is this not working
     //top.postMessage('from-revealjs', '*');
-
-    //alert(ifr.getAttribute('src'));
-    //alert(fragment.getAttribute ('data-fragment-index'));
 };
