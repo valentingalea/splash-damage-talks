ea2d981b37c733fdc62fdab19818f8159e41f08b
valentingalea <valentingalea@gmail.com>
2019-03-20 23:06:48 +0000

    better handling of surf navigation

--- reveal/custom-fragments.js
+++ reveal/custom-fragments.js
@@ -1,19 +1,20 @@
 var CustomFragments = (function() {
     Reveal.addEventListener('fragmentshown', function(event) {
 	    // event.fragment = the fragment DOM element
-        dealWithFragments(event.fragment);
+        dealWithFragments(event.fragment, true);
     });
     Reveal.addEventListener('fragmenthidden', function(event) {
-        dealWithFragments(event.fragment);
+        dealWithFragments(event.fragment, false);
     });
 })();
 
-function dealWithFragments (fragment) {
+function dealWithFragments(fragment, appear) {
     var iframe = document.querySelector("div.slide-background.present > iframe:nth-child(1)");
     if (!iframe) return;
 
     // add 1 because we skip the start aka overview
-    const id = Number(fragment.getAttribute ('data-fragment-index')) + 1;
+    const frag_id = Number(fragment.getAttribute ('data-fragment-index')); 
+    const id = frag_id + 1;
 
     // format of iframe url is: index.html#X.Y
     const href = iframe.contentWindow.location.href;
@@ -21,6 +22,10 @@ function dealWithFragments (fragment) {
     const new_href = href.substr(0, h + 2) + '.' + id;
     
     iframe.contentWindow.location.href = new_href;
+    if (!appear && frag_id == 0) {
+        // reset to overview slide when fragments are all hidden
+        iframe.contentWindow.location.href = href.substr(0, h + 2); 
+    }
     //console.log(new_href);
     
     //top.postMessage('from-revealjs', '*');
