03579a240cb22f7d268bccd6978aa58587df8b1a
valentingalea <valentingalea@gmail.com>
2019-03-21 09:38:17 +0000

    add menu plugin

--- reveal/plugins.js
+++ reveal/plugins.js
@@ -1,7 +1,30 @@
+// https://github.com/denehyg/reveal.js-menu
+menu: {
+    side: 'left',
+    width: 'normal',
+    numbers: true,
+    useTextContentForMissingTitles: true,
+    hideMissingTitles: false,
+    markers: true,
+    custom: false,
+    themes: false,
+    themesPath: 'css/theme/',
+    transitions: true,
+    openButton: true,
+    openSlideNumber: false,
+    keyboard: true,
+    sticky: false,
+    autoOpen: true,
+    delayInit: false,
+    openOnInit: false,
+    loadIcons: true
+},
+
 //
 // plugins
 //
 dependencies: [
-    { src: 'custom-fragments.js' }
+     { src: 'custom-fragments.js' }
+    ,{ src: 'node_modules/reveal.js-menu/menu.js' }
 
     , // default plugins past this point; also add new-line
