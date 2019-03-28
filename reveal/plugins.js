fc568b8de98519ead5ccee8683759bb45f6666e8
valentingalea <valentingalea@gmail.com>
2019-03-28 08:40:13 +0000

    add elapsed-time-bar plugin

--- reveal/plugins.js
+++ reveal/plugins.js
@@ -20,12 +20,18 @@ menu: {
     loadIcons: false
 },
 
+// https://github.com/tkrkt/reveal.js-elapsed-time-bar
+allottedTime: 40 * 60 * 1000, // minutes
+progressBarHeight: 5,
+barColor: 'rgb(66, 175, 250)',
+
 //
 // plugins
 //
 dependencies: [
      { src: 'plugins/custom-fragments.js' }
     ,{ src: 'plugins/header.js' }
+    ,{ src: 'plugins/elapsed-time-bar.js' }
     ,{ src: 'plugins/reveal.js-menu/menu.js' }
 
     , // default plugins past this point; also add new-line
